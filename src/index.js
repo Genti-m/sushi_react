44ad190a98d9adf710ff9e14d13d147972d0a532
Ney-lo <genti.musaj@tesiengineering.com>
2023-05-30 10:34:03 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <p>ciao mondo</p>
-  </React.StrictMode>
+  <p>ciao mondo</p>
 );
