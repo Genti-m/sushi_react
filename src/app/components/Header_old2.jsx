64758f6de94d8e29bed25771775a022c38e923f2
Genti <genti.musaj@tesiengineering.com>
2023-05-30 20:59:33 +0200

    last checkpoint, header nearly done

--- src/app/components/Header_old2.jsx
+++ src/app/components/Header_old2.jsx
@@ -1,74 +1,43 @@
 import React, { useEffect } from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import '../../styles/index.scss'; // Import the main styles file
 
-// Import the _header.scss file
-import '../../styles/_header.scss';
-
-// Create a global style to apply the imported styles
-const GlobalStyle = createGlobalStyle`
-  @import '../../styles/_header.scss';
-`;
-
-// Define the styled components
-const HeaderWrapper = styled.header`
-  /* Any other styles for the header wrapper */
-`;
+import searchIcon from '../../assets/svg/search.svg';
+import menuIcon from '../../assets/svg/menu.svg';
 
 const Header = () => {
   useEffect(() => {
     // Create the header menu items
-    const navbarItems = [
-      { text: 'Logo', href: '#logo', islogo: true },
+    const headerMenuItems = [
       { text: 'Menu', href: '#menu' },
       { text: 'Food', href: '#food' },
       { text: 'Services', href: '#services' },
       { text: 'About Us', href: '#about-us' },
     ];
 
-    const navbar = document.createElement('ul');
-    navbar.classList.add('header__menu');
-    navbar.setAttribute('data-aos', 'fade-down');
+    const headerMenu = document.createElement('ul');
+    headerMenu.classList.add('header__menu');
+    headerMenu.setAttribute('data-aos', 'fade-down');
 
-    navbarItems.forEach((item) => {
+    headerMenuItems.forEach((item) => {
       const listItem = document.createElement('li');
       const anchor = document.createElement('a');
       anchor.href = item.href;
       anchor.textContent = item.text;
-
-      // Check if the item is the logo
-      if (item.islogo) {
-        const logoContainer = document.createElement('div');
-        logoContainer.classList.add('header__logo');
-
-        const logoOverlay = document.createElement('div');
-        logoOverlay.classList.add('header__logo-overlay');
-
-        const logoText = document.createElement('h4');
-        logoText.textContent = item.text;
-
-        logoContainer.appendChild(logoOverlay);
-        logoContainer.appendChild(logoText);
-
-        anchor.appendChild(logoContainer);
-      }
-      
       listItem.appendChild(anchor);
-      navbar.appendChild(listItem);
+      headerMenu.appendChild(listItem);
     });
 
-    /*    USELESS NOW
     // Create the search icon item
     const searchIconItem = document.createElement('li');
     const searchIconImg = document.createElement('img');
     searchIconImg.src = searchIcon;
     searchIconImg.alt = 'search';
     searchIconItem.appendChild(searchIconImg);
-    navbar.appendChild(searchIconItem);
-    */
+    headerMenu.appendChild(searchIconItem);
 
     // Append the header menu to the header element
     const header = document.querySelector('header');
-    header.appendChild(navbar);
+    header.appendChild(headerMenu);
 
     // Create the mobile menu icon item
     const mobileMenuIconItem = document.createElement('li');
@@ -88,12 +57,33 @@ const Header = () => {
   }, []);
 
   return (
-    <>
-      <GlobalStyle />
-      <HeaderWrapper className="header__wrapper">
-        {/* Any other content in the header */}
-      </HeaderWrapper>
-    </>
+    <header>
+      <nav>
+        <ul className="header__menu" data-aos="fade-down">
+          <li>
+            <a href="#menu">Menu</a>
+          </li>
+          <li>
+            <a href="#food">Food</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#about-us">About Us</a>
+          </li>
+          <li>
+            <img src={searchIcon} alt="search" />
+          </li>
+        </ul>
+
+        <ul className="header__menu-mobile" data-aos="fade-down">
+          <li>
+            <img src={menuIcon} alt="menu" />
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './app/pages/Home';
 import './styles/index.scss';
+import 'aos/dist/aos.css'; // Import the AOS CSS file
+import AOS from 'aos'; // Import the AOS library
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Home />
 );
+
+AOS.init(); // Initialize AOS after rendering the component
